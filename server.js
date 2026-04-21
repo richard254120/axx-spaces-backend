b26f3d3358d503133a95981239a817b6626eabc4
richard254120 <ogudarichard254@gmail.com>
2026-04-21 22:38:25 +0300

    fix mongodb connection duplicate

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
    MIDDLEWARE
 ========================= */
 app.use(cors({
-  origin: "*", // allow frontend (Netlify)
+  origin: "*",
 }));
 
 app.use(express.json());
@@ -33,12 +33,9 @@ app.use("/", propertyRoutes);
 /* =========================
    MONGODB CONNECTION
 ========================= */
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log("✅ MongoDB Connected"))
-.catch(err => console.log("❌ DB Error:", err));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ MongoDB Connected"))
+  .catch(err => console.log("❌ DB Error:", err));
 
 /* =========================
    START SERVER
