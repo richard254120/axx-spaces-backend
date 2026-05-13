2fe4a460f684b9b4f7887b94e18be8f8e58aa38c
richard254120 <ogudarichard254@gmail.com>
2026-05-13 10:09:49 +0300

    Hardcode email credentials to test Render

--- server.js
+++ server.js
@@ -23,8 +23,8 @@ console.log("✅ Middleware configured");
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASSWORD,
+    user: "ogudarichard254@gmail.com",
+    pass: "uzqbcuzrzhttzeyl",
   },
 });
 
