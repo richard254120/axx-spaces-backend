e6cf83a35675232ebb7a9a9a476b2d2ee4023156
richard254120 <ogudarichard254@gmail.com>
2026-05-25 23:48:25 +0300

    Updated feature

--- services/tourismService.js
+++ services/tourismService.js
@@ -414,7 +414,7 @@ export async function registerProviderAndListing(body, files = []) {
     email: ownerEmail.toLowerCase(),
     phone: ownerPhone,
     password: await bcrypt.hash(password, 10),
-    role: "landlord",
+    role: "tourism_provider",
   });
 
   const listing = await buildAndSaveListing(
