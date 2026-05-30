faf68430763d718aa6357171fe9be3c3689a5cd5
richard254120 <ogudarichard254@gmail.com>
2026-05-30 11:13:31 +0300

    Updated project

--- models/Notification.js
+++ models/Notification.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 const notificationSchema = new mongoose.Schema({
   type: {
@@ -48,4 +48,6 @@ const notificationSchema = new mongoose.Schema({
   },
 });
 
-module.exports = mongoose.model("Notification", notificationSchema);
+const Notification = mongoose.models.Notification || mongoose.model("Notification", notificationSchema);
+
+export default Notification;
