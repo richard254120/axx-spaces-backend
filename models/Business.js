94500cab112c0efed9c5ca3cacd536a3858665dc
richard254120 <ogudarichard254@gmail.com>
2026-05-30 15:39:18 +0300

    Force update

--- models/Business.js
+++ models/Business.js
@@ -15,6 +15,19 @@ const businessSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  yearEstablished: {
+    type: Number,
+    min: 1900,
+    max: new Date().getFullYear(),
+  },
+  employeeCount: {
+    type: String,
+    enum: ["1-10", "11-50", "51-200", "201-500", "500+"],
+  },
+  priceRange: {
+    type: String,
+    enum: ["$", "$$", "$$$", "$$$$"],
+  },
   categories: [{
     type: String,
     required: true,
