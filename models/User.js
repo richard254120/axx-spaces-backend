091d8b8263ca17b489d583c18ececbf1ba0e4510
richard254120 <ogudarichard254@gmail.com>
2026-06-04 11:14:38 +0300

    Updated homepage

--- models/User.js
+++ models/User.js
@@ -30,10 +30,10 @@ const userSchema = new mongoose.Schema(
       type: String,
       default: "",
     },
-    // ✅ ROLE IDENTIFICATION — added "seller"
+    // ✅ ROLE IDENTIFICATION — added "seller" and "team"
     role: {
       type: String,
-      enum: ["user", "mover", "admin", "landlord", "seller", "seller"],
+      enum: ["user", "mover", "admin", "landlord", "seller", "team"],
       default: "user",
     },
 
